949908b6c50e22aa0e13f25ddab1cacc6da52c67
Emric <emric.mansson@gmail.com>
2020-02-16 08:57:40 +0000

    Add tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,6 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
+require("css/application.scss")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
