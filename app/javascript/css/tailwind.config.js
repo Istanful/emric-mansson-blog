7f546cd04dbd8041d2c9b82cf01889f5bcb23010
Emric <emric.mansson@gmail.com>
2020-02-17 20:55:24 +0000

    Add homepage wallpaper

--- app/javascript/css/tailwind.config.js
+++ app/javascript/css/tailwind.config.js
@@ -2,9 +2,6 @@ const { colors } = require('tailwindcss/defaultTheme');
 
 module.exports = {
   theme: {
-    boxShadow: {
-      lg: '0 10px 15px -3px rgba(0, 0, 0, 0.05), 0 4px 6px -2px rgba(0, 0, 0, 0)'
-    },
     extend: {
       colors: {
         blue: '#3b57bf',
