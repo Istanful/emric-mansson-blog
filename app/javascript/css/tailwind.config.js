99db3ab7d054e5d4a4d17c0b5e425595aefa69a0
Emric <emric.mansson@gmail.com>
2020-02-17 20:12:47 +0000

    Add introduction text

---
+++ app/javascript/css/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        blue: '#3b57bf',
+      }
+    },
+    screens: {
+      sm: '640px'
+    },
+    container: {
+      center: true,
+    }
+  },
+  variants: {},
+  plugins: [],
+}
--- postcss.config.js
+++ postcss.config.js
@@ -3,12 +3,10 @@ module.exports = {
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
+      autoprefixer: { flexbox: 'no-2009' },
       stage: 3
     }),
-    require('tailwindcss'),
+    require('tailwindcss')('./app/javascript/css/tailwind.config.js'),
     require('autoprefixer'),
   ]
 }
