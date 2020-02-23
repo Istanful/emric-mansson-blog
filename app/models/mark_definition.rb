8ed576c8651c289d2fbba23a09afafb6652b1c50
Emric <emric.mansson@gmail.com>
2020-02-23 15:51:31 +0000

    Reimplement styles for posts

--- app/models/mark_definition.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module MarkDefinition
-  def self.render(definition, content)
-    renderer = "MarkDefinition::#{definition["_type"].camelize}".constantize
-    renderer.new(definition, content).render
-  end
-end
