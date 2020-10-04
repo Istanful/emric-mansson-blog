6be1c11e68c16751242df342691db3f0133a6feb
Emric <emric.mansson@gmail.com>
2020-10-04 21:09:53 +0100

    Add youtube block type

---
+++ app/models/portable_text/youtube.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class PortableText::Youtube
+  include PortableText::Renderer
+
+  RATIO = 9 / 16.0
+
+  def render
+    iframe_classes = "w-full absolute inset-0 h-full"
+    iframe = content_tag :iframe, '', src: json["url"], class: iframe_classes
+    content_tag :div,
+                iframe,
+                style: "padding-bottom: #{RATIO * 100}%", class: 'relative'
+  end
+end
